c079ee81b66bb33b64feee5054a36db309678b99
mandyle08 <amandale@cmail.carleton.ca>
2019-01-10 23:05:32 -0500

    moved adjust below the moods

--- MoodChart/MoodChart/mainwindow.cpp
+++ MoodChart/MoodChart/mainwindow.cpp
@@ -11,15 +11,15 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->table->horizontalHeader()->setSectionResizeMode(0, QHeaderView::Fixed);
     ui->table->verticalHeader()->setSectionResizeMode(0, QHeaderView::Fixed);
 
-    ui->table->setSizeAdjustPolicy(QAbstractScrollArea::AdjustToContents);
-    adjustSize();
-
     addMood(Mood(QColor(250,255,124), QString("Happy")));
     addMood(Mood(QColor(255,128,0), QString("Average")));
     addMood(Mood(QColor(47,131,255), QString("Depressed/Anxious")));
     addMood(Mood(QColor(117,123,134), QString("Tired")));
     addMood(Mood(QColor(36,220,79), QString("Sick")));
     addMood(Mood(QColor(252,83,83), QString("Angry")));
+
+    ui->table->setSizeAdjustPolicy(QAbstractScrollArea::AdjustToContents);
+    adjustSize();
 }
 
 MainWindow::~MainWindow()
