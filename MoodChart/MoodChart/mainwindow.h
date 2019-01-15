ead0f84da864ac7b80857a1ee927a4791085af40
mandyle08 <amandale@cmail.carleton.ca>
2019-01-15 14:56:30 -0500

    BIG change

--- MoodChart/MoodChart/mainwindow.h
+++ MoodChart/MoodChart/mainwindow.h
@@ -2,7 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QInputDialog>
+#include <QDebug>
+#include <QTableWidget>
+#include <QPalette>
 #include "mood.h"
+
 
 namespace Ui {
 class MainWindow;
@@ -18,12 +23,19 @@ public:
 
 private slots:
     void on_action_Quit_triggered();
+    void on_table_customContextMenuRequested(const QPoint &pos);
+    void prompt_for_mood();
+    void slot_mood_choice(const QString &choice);
 
 private:
     Ui::MainWindow *ui;
     QVector <Mood> moods;
     void addMood(const Mood &mood);
     //void removeMood();
+    void setColor(const int &x, const int &y, const QColor &color);
+    void setMood(const int &x, const int &y, const Mood &mood);
+    void populateColor(const int &x, const int &y,const Mood &mood);
+    int selectedMoodIndex;
 };
 
 
